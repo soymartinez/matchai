cf5a842dc1c0a9dd6f1ec8f5552750684805d67e
omgismartinez <martnzomg@gmail.com>
2023-02-19 19:13:40 -0600

    🚀 feat: initial proyect

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <main
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh'
+      }}
+    >
+      <h1>Match AI</h1>
+    </main >
+  )
+}
